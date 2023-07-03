fa673be73e42e60463799913eb8935f3cc4c0808
Scorpio_I4 <18556134754@163.com>
2023-07-04 01:21:51 +0800

    chore: init commit

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(
+  <>
+    <div>this is a jsx</div>
+  </>
+);
