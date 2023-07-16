45eddab9364823a169638f8b82c84141749b8d7d
Scorpio <18556134754@163.com>
2023-07-17 00:27:03 +0800

    feat: redux over

--- src/store/action/modules/app.js
+++ src/store/action/modules/app.js
@@ -1,4 +1,5 @@
 import * as TYPES from '../../action-types';
+import { delay } from '../../../utils';
 
 export const appAction = {
   titleChange() {
@@ -7,8 +8,12 @@ export const appAction = {
     };
   },
   countChange() {
-    return {
-      type: TYPES.APP_COUNT_CHANGE
+    // 适配 redux-thunk 的语法，支持异步 action【或者使用 redux-promise 也可以实现异步 action】
+    return async (dispatch) => {
+      await delay(2000);
+      dispatch({
+        type: TYPES.APP_COUNT_CHANGE
+      });
     };
   }
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
+import reduxLogger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
 
 // store 容器
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(reduxLogger, reduxThunk));
 
 /**
  * store 每一次 dispatch 都会将 reducer 执行一遍，store 初始化时会在 redux 内部执行一遍 reducer，只不过 action.type 是
